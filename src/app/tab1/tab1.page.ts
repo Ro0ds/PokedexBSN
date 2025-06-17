2b2ed45ca949beef9285c7601e0deb60d47e681c
Rodrigo <digo_gabriel97@yahoo.com.br>
2025-06-17 20:33:15 -0300

    feat pesquisa do pokemon adicionando um card com seu nome e foto + botão de info

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,23 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { PokeService } from '../services/poke.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: false,
+  encapsulation: ViewEncapsulation.None,
 })
 export class Tab1Page {
+  constructor(private pokeService: PokeService) {}
+  
   nomePokemon: string = '';
+  pokemon: any = null;
+  temaEscuro: boolean = false;
+  animarIcone: boolean = false;
   
   procurarPokemon() {
-    if(!this.nomePokemon.trim()) {
-      console.log('digite um nome');
+    const nome = this.nomePokemon.trim().toLowerCase();
+    
+    if(!nome) {
+      console.log('vazio');
       return;
     }
 
-    const nome = this.nomePokemon.trim().toLowerCase();
-    console.log('procurando:', nome);
+    this.pokeService.buscarPokemon(nome).subscribe({
+      next: (res) => {
+        console.log('dados temp do pokemon:', res);
+        this.pokemon = {
+          nome: res.name,
+          imagem: res.sprites?.other['official-artwork']?.front_default || res.sprites.front_default
+        };
+      },
+      error: (err) => {
+        console.error('pokemon nao encontrado:', err);
+        this.pokemon = null;
+      }
+    })
+  }
 
-    // call service
+  alternarTema() {
+    this.temaEscuro = !this.temaEscuro;
+
+    if(this.temaEscuro) {
+      document.body.classList.remove('light');
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+      document.body.classList.add('light');
+    }
+
+    this.animarIcone = true;
+    setTimeout(() => this.animarIcone = false, 500);
   }
 }

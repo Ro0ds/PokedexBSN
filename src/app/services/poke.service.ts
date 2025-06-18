9b9e1697d5f2d65e31104d4351c593c5a80d9c80
Rodrigo <digo_gabriel97@yahoo.com.br>
2025-06-18 19:48:53 -0300

    feat + refactor busca de pokemon agora é feita com base no cache de memoria e cache local, para depois ir para a API

--- src/app/services/poke.service.ts
+++ src/app/services/poke.service.ts
@@ -1,16 +1,59 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokeService {
-  private baseUrl = 'https://pokeapi.co/api/v2/pokemon'
+  private baseUrl = 'https://pokeapi.co/api/v2/pokemon';
+  private cache: Map<string, any> = new Map();
 
   constructor(private http: HttpClient) { }
 
   buscarPokemon(nome: string): Observable<any> {
-    return this.http.get(`${this.baseUrl}/${nome.toLowerCase()}`);
+    const nomeLower = nome.toLowerCase();
+
+    if(this.cache.has(nomeLower)) {
+      return of(this.cache.get(nomeLower));
+    }
+
+    const cacheLocal = this.buscarNoCacheLocal(nomeLower);
+    if(cacheLocal) {
+      this.cache.set(nomeLower, cacheLocal);
+      return of(cacheLocal);
+    }
+
+    return this.http.get(`${this.baseUrl}/${nomeLower}`).pipe(
+      tap(dados => {
+        this.cache.set(nomeLower, dados);
+        this.salvarEmCacheLocal(dados);
+      })
+    );
+  }
+
+  getUltimosPokemons(): any[] {
+    const dados = localStorage.getItem('ultimosPokemons');
+    return dados ? JSON.parse(dados) : [];
+  }
+
+  private buscarNoCacheLocal(nome: string): any | null {
+    const lista: any[] = JSON.parse(localStorage.getItem('ultimosPokemons') || '[]');
+    return lista.find(p => p.name.toLowerCase() === nome) || null;
+  }
+
+  private salvarEmCacheLocal(pokemon: any) {
+    const chave = 'ultimosPokemons';
+    const nome = pokemon.name.toLowerCase();
+
+    let lista: any[] = JSON.parse(localStorage.getItem(chave) || '[]');
+    lista = lista.filter((p) => p.name.toLowerCase() !== nome);
+    lista.unshift(pokemon);
+
+    if(lista.length > 10) {
+      lista = lista.slice(0, 10);
+    }
+
+    localStorage.setItem(chave, JSON.stringify(lista));
   }
 }

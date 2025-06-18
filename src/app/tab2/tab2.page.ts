931a9981de14214a1acadf80903dc2947a946a0e
Rodrigo <digo_gabriel97@yahoo.com.br>
2025-06-18 19:46:12 -0300

    feat adição da logica do tab2 (infos de pokemon) e da lógica de favoritos

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DadosService } from '../services/dados.service';
+import { FavoritosService } from '../services/favoritos.service';
+import { Utils } from '../Utilities/utils';
 
 @Component({
   selector: 'app-tab2',
@@ -7,7 +10,34 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class Tab2Page {
+  dadosPokemonBruto: any = null;
+  dadosPokemon: any = null;
+  favorito: boolean = false;
+  favoritos: any[] = [];
 
-  constructor() {}
+  constructor(
+    private dadosService: DadosService,
+    private favoritoService: FavoritosService,
+    private utilities: Utils) { }
 
+  // arrumando os dados na inicialização da página, se ele existir
+  ionViewWillEnter() {
+    this.dadosPokemonBruto = this.dadosService.getDados();
+    if(this.dadosPokemonBruto) {
+      this.dadosPokemon = this.utilities.mapearPokemon(this.dadosPokemonBruto);
+      this.favorito = this.favoritoService.isFavorito(this.dadosPokemon.nome);
+    }
+
+    this.favoritos = this.favoritoService.getFavoritos();
+  }
+
+  alternarFavorito() {
+    if(this.favorito) {
+      this.favoritoService.removerFavoritos(this.dadosPokemon.nome);
+      this.favoritos = this.favoritoService.getFavoritos();
+    } else {
+      this.favoritoService.adicionarFavorito(this.dadosPokemon);
+    }
+    this.favorito = !this.favorito;
+  }
 }
